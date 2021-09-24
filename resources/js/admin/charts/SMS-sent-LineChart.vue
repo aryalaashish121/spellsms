bafa839f67988b2ee69b632050cc207659f56793
ulquiorra7 <ciferulquiorra008@gmail.com>
2021-09-24 17:41:00 +0545

    Account Control Panel Design updated

---
+++ resources/js/admin/charts/SMS-sent-LineChart.vue
@@ -0,0 +1,33 @@
+<script>
+import { Line } from "vue-chartjs";
+
+export default {
+  extends: Line,
+  mounted() {
+    // Overwriting base render method with actual data.
+    this.renderChart({
+      labels: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ],
+      datasets: [
+        {
+          label: "SMS Sent",
+          backgroundColor: "#BBDEFB",
+          data: [5, 10, 20, 30, 50, 100, 70, 80, 30, 0, 0, 0],
+        },
+      ],
+    });
+  },
+};
+</script>
---
+++ resources/js/components/ToolTips.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-tooltip right>
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn v-on="on" v-bind="attrs" fab dark x-small :color="color">
+        <v-icon>{{ icon }}</v-icon>
+      </v-btn>
+    </template>
+    <span class="text-xs">{{ title }}</span>
+  </v-tooltip>
+</template>
+
+<script>
+export default {
+  props: { icon: String, title: String, color: String },
+};
+</script>
